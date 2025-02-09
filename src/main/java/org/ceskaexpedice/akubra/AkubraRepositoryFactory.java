0670f37e756cad1db1f42f2a6fe440e740f154b2
ppodsednik <petr.podsednik@gmail.com>
2025-02-09 09:20:57 +0100

    add license header

--- src/main/java/org/ceskaexpedice/akubra/AkubraRepositoryFactory.java
+++ src/main/java/org/ceskaexpedice/akubra/AkubraRepositoryFactory.java
@@ -1,18 +1,18 @@
-/**
- * Copyright ©2023 Accenture and/or its affiliates. All Rights Reserved.
- * <p>
- * Permission to any use, copy, modify, and distribute this software and
- * its documentation for any purpose is subject to a licensing agreement
- * duly entered into with the copyright owner or its affiliate.
- * <p>
- * All information contained herein is, and remains the property of Accenture
- * and/or its affiliates and its suppliers, if any.  The intellectual and
- * technical concepts contained herein are proprietary to Accenture and/or
- * its affiliates and its suppliers and may be covered by one or more patents
- * or pending patent applications in one or more jurisdictions worldwide,
- * and are protected by trade secret or copyright law. Dissemination of this
- * information or reproduction of this material is strictly forbidden unless
- * prior written permission is obtained from Accenture and/or its affiliates.
+/*
+ * Copyright (C) 2025 Inovatika
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.ceskaexpedice.akubra;
 
--- src/main/java/org/ceskaexpedice/akubra/core/repository/impl/RepositoryObjectImpl.java
+++ src/main/java/org/ceskaexpedice/akubra/core/repository/impl/RepositoryObjectImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Pavel Stastny
+ * Copyright (C) 2025 Inovatika
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
